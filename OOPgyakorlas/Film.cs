006a40a073bd3abffe18ac137d5b4aefdfe47159
Petrik Diak <diak@petrik.hu>
2025-02-07 12:59:56 +0100

    Elkészítettema filmes feladat nagyrészét, a bool-os rész nem ment :(

---
+++ OOPgyakorlas/Film.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPgyakorlas
+{
+	internal class Film
+	{
+		private string cim;
+		private string rendezo;
+		private int hosszPercekben;  //Adattagok
+		private string mufaj;
+		private bool megjelent;
+
+		public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)  //Konstruktor (itt minden tag)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			this.hosszPercekben = hosszPercekben;
+			this.mufaj = mufaj;
+			this.megjelent = megjelent;
+		}
+
+		public Film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;      //Konstruktor (csak 2 tag)
+			hosszPercekben = 120;
+			mufaj = "ismeretlen";
+			megjelent = false;
+		}
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }  //Propertik
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelent { get => megjelent; set => megjelent = value; }
+
+
+
+		public void Jatszas()
+		{
+			if (this.Megjelent)
+			{
+                Console.WriteLine("A film elkezdődött.");
+            }
+			else
+			{
+                Console.WriteLine("A film nem kezdődött el.");
+            }
+		}
+
+		public void MufajModositas(string ujMufaj)
+		{
+			Mufaj = ujMufaj;
+		}
+
+		public void HosszNovelese(int percek)
+		{
+			HosszPercekben += percek;
+		}
+
+		public override string? ToString()   //Jobb klikk, quick action 
+		{
+			return $"A film címe: {this.Cim}, rendezője: {this.Rendezo}, Hossza(percben): {this.HosszPercekben}, műfaja: {this.Mufaj} és hogy megjelent-e: {this.Megjelent}";
+		}
+	}
+
+
+
+
+}
--- OOPgyakorlas/Program.cs
+++ OOPgyakorlas/Program.cs
@@ -4,15 +4,42 @@
 	{
 		static void Main(string[] args)
 		{
-			Konyv konyv1 = new ("A kis herceg", "Antoine", 1943, 96, 2500);
-			Konyv konyv2 = new ("Ez még készül", "Én");
 
-            Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+            Console.WriteLine("1.Feladat");
+            Konyv konyv1 = new("A kis herceg", "Antoine", 1943, 96, 2500);
+			Konyv konyv2 = new("Ez még készül", "Én");
+
+			Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
 
 			konyv1.Kedvezmeny(20);
 			konyv1.Arnoveles(1000);
 
 			Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
-		}
+
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("");
+
+
+
+
+            Film film1 = new("Nagymenők", "Scorsese", 180, "Gengszterfilm", true);
+
+			Film film2 = new("Keresztapa", "Francis Ford Copolla");
+
+			Console.WriteLine(film1.ToString());
+            Console.WriteLine("");
+			film1.HosszNovelese(20);            //Itt növeltem a film perceit
+			Console.WriteLine(film1.ToString());
+
+            Console.WriteLine("");
+
+            Console.WriteLine(film2.ToString());
+            Console.WriteLine("");
+			film2.MufajModositas("Kultuszfilm"); //Itt módosítottam a műfajaát
+			Console.WriteLine(film2.ToString());	
+
+
+        }
 	}
 }
