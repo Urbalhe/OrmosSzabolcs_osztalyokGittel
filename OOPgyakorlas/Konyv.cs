c1908dfcf43a5d28592a8aef80dbd9856011b502
Petrik Diak <diak@petrik.hu>
2025-02-07 11:17:36 +0100

    Könyv nevű osztály létrehozva 2 konstruktorral, 3 metódussal

---
+++ OOPgyakorlas/Konyv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPgyakorlas
+{
+	internal class Konyv
+	{
+
+		private string cim;
+		private string szerzo;
+		private int kiadasEve;
+		private int oldalSzam;
+		private int ar;
+
+		public string Cim { get => cim; set => cim = value; }       // 3. Jobb klikk + quick actions, encapsulate but still use field
+		public string Szerzo { get => szerzo; set => szerzo = value; }
+		public int KiadasEve { get => kiadasEve; set => kiadasEve = value; }
+		public int OldalSzam { get => oldalSzam; set => oldalSzam = value; }
+		public int Ar { get => ar; set => ar = value; }
+
+		public Konyv(string cim, string szerzo, int kiadasEve, int oldalSzam, int ar)
+		{
+			this.cim = cim;          // 1. Jobb klikk + quick actions
+			this.szerzo = szerzo;
+			this.kiadasEve = kiadasEve;
+			this.oldalSzam = oldalSzam;
+			this.ar = ar;
+		}
+
+		public Konyv(string cim, string szerzo)     //2. ALt + Enter
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			kiadasEve = DateTime.Now.Year;
+			oldalSzam = 200;
+			ar = 3000;
+		}
+
+		public void Arnoveles(int osszeg)
+		{
+			ar += osszeg;
+		}
+
+		public void Kedvezmeny(double szazalek)
+		{
+			ar -= Convert.ToInt16(ar * (szazalek / 100));
+		}
+
+		public int HanyOldalMaradt(int elolvasott)
+		{
+			return OldalSzam - elolvasott;
+		}
+
+		public override string? ToString()
+		{
+			return $"{this.cim} - {this.szerzo} / {this.OldalSzam} / {this.kiadasEve} - {this.ar}";
+		}
+	}
+}
+
+
